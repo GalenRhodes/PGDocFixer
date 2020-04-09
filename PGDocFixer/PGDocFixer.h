a7b5f893be13d6492277d8adf77fc8a571bb40ba
Galen Rhodes <galenrhodes@me.com>
2020-04-09 11:48:20 -0400

    Initial Commit

---
+++ PGDocFixer/PGDocFixer.h
@@ -0,0 +1,19 @@
+//
+//  PGDocFixer.h
+//  PGDocFixer
+//
+//  Created by Galen Rhodes on 4/9/20.
+//  Copyright © 2020 Project Galen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PGDocFixer.
+FOUNDATION_EXPORT double PGDocFixerVersionNumber;
+
+//! Project version string for PGDocFixer.
+FOUNDATION_EXPORT const unsigned char PGDocFixerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PGDocFixer/PublicHeader.h>
+
+
