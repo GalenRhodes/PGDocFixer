a38c9a2e62735efb699ae05251251920aa584e0c
Galen Rhodes <galenrhodes@me.com>
2020-04-30 11:26:37 -0400

    Added .gitignore

--- build/PGDocFixer.build/Release/PGDocFixer.build/DerivedSources/PGDocFixer_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char PGDocFixerVersionString[];
- extern const double PGDocFixerVersionNumber;
-
- const unsigned char PGDocFixerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:PGDocFixer  PROJECT:PGDocFixer-1" "\n";
- const double PGDocFixerVersionNumber __attribute__ ((used)) = (double)1.;
--- build/PGDocFixer.build/Release/PGDocFixer.build/Objects-normal/x86_64/PGDocFixer_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/grhodes/Projects/2020/SwiftProjects/PGDocFixer/build/PGDocFixer.build/Release/PGDocFixer.build/DerivedSources/PGDocFixer_vers.c
